90f53ffa72f0730f38f59a95f73b7abae284f97a
scain-ui <scain@midland.k12.ia.us>
2025-12-03 16:33:14 +0000

    1

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
+class Agable {
+    constructor(year){
+        this.year = year;
+    }
+
+    age() {
+        const currentYear = new Date().getFullYear(); 
+        return currentYear - this.year;
+    }
+
+}
+
+
+
 class Company{
     constructor(name, taxId, yearEstablished, taxRate){
+        super(yearEstablished);
         this.name = name;
         this.id = taxId;
-        this.year = yearEstablished;
         this.taxRate = taxRate;
     }
 }
 
 class Person{
     constructor(name, ssn, birthYear, taxRate){
+        super(birthYear);
         this.name = name;
         this.id = ssn;
-        this.year = birthYear;
         this.taxRate = taxRate;
     }
 }
 
 class Car{
     constructor(model, vin, year){
+        super(year);
         this.name = model;
         this.id = vin;
         this.year = year;
